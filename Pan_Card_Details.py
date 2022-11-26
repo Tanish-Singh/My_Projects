dcaa858a525cebb75394b536d692d3a68d12076c
Tanish-Singh <118847320+Tanish-Singh@users.noreply.github.com>
2022-11-27 00:18:59 +0530

    Add files via upload

---
+++ Pan_Card_Details.py
@@ -0,0 +1,34 @@
+import http.client
+import json
+from prettytable import PrettyTable
+mypan=input('enter your pan card=')
+conn = http.client.HTTPSConnection("pan-card-verification1.p.rapidapi.com")
+
+payload = "{\n    \"task_id\": \"74f4c926-250c-43ca-9c53-453e87ceacd1\",\n    \"group_id\": \"8e16424a-58fc-4ba4-ab20-5bc8e7c3c41e\",\n    \"data\": {\n        \"id_number\": \""+mypan+"\"\n    }\n}"
+
+headers = {
+    'content-type': "application/json",
+    'X-RapidAPI-Key': "6bf16b1a6fmsh0138bdf82af8ba9p169593jsn7732a7b8c59d",
+    'X-RapidAPI-Host': "pan-card-verification1.p.rapidapi.com"
+    }
+
+conn.request("POST", "/v3/tasks/sync/verify_with_source/ind_pan", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+mydata=json.loads(data.decode("utf-8"))
+check=mydata['status']
+if check=='completed':
+    adhar=mydata['result']['source_output']['aadhaar_seeding_status']
+    fname=mydata['result']['source_output']['first_name']
+    lname=mydata['result']['source_output']['last_name']
+    noc=mydata['result']['source_output']['name_on_card']
+
+    head=['aadhaar_seeding_status','First_name','Last_name','name_on_card']
+    myheading=PrettyTable(head)
+    data=[adhar,fname,lname,noc]
+    myheading.add_row(data)
+    print(myheading)
+else:
+    print('invalid pan card')
